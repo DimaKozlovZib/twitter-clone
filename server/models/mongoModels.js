cd4571f6540f26fc7cb6329e05e42cfe10237dff
Dima Kozlov <zibocinfinity@gmail.com>
2023-08-12 14:49:19 +0300

    вычисленние оценки сообщения

--- server/models/mongoModels.js
+++ server/models/mongoModels.js
@@ -1,6 +1,8 @@
 const { Schema, model } = require('mongoose')
 
 const U_M_Model = 'user_message';
+const maxPositiveGrade = 4;
+const minNegativeGrade = -2;
 
 const user_messageShema = new Schema({
     userId: {
@@ -18,7 +20,7 @@ const user_messageShema = new Schema({
     grade: {
         type: Number,
         default: 0,
-        max: 10, min: -10
+        max: maxPositiveGrade, min: minNegativeGrade
     },
     hasLike: {
         type: Boolean,
@@ -32,10 +34,12 @@ const user_messageShema = new Schema({
     {
         statics: {
             async setLike(userId, messageId, data) {//data: true or false
-                return await model(U_M_Model).updateOne({ userId, messageId }, { hasLike: data })
+                await model(U_M_Model).updateOne({ userId, messageId }, { hasLike: data })
+                this.gradeCalculate(userId, messageId)
             },
             async updateMessageShown(userId, messageId) {
-                return await model(U_M_Model).updateOne({ userId, messageId }, { $inc: { showCount: 1 } })
+                await model(U_M_Model).updateOne({ userId, messageId }, { $inc: { showCount: 1 } })
+                this.gradeCalculate(userId, messageId)
             },
             async setCommentMood(userId, messageId, data) {//data: 'positively', 'negatively', 'neutral'
                 const object = await model(U_M_Model).updateOne({ userId, messageId }, { commentMood: data })
@@ -44,8 +48,31 @@ const user_messageShema = new Schema({
                     const newAction = new USER_MESSAGE({ userId, messageId, showCount: 1, commentMood: data, })
                     await newAction.save()
                 }
+                this.gradeCalculate(userId, messageId)
+            },
+            async gradeCalculate(userId, messageId) {
+                try {
+                    const object = await model(U_M_Model).findOne({ userId, messageId })
+                    const { hasLike, commentMood } = object;
+                    let gradeResult = 0;
+
+                    //оцениваем настроение комментария
+                    if (commentMood === 'positively') {
+                        gradeResult += 2;
+                    } else if (commentMood === 'negatively') {
+                        gradeResult -= 2;
+                    }
+                    // принимаем в расчет лайк
+                    if (hasLike) {
+                        gradeResult += 2;
+                    }
+
+                    await object.updateOne({ grade: gradeResult })
+                } catch (error) {
+                    console.log(error)
+                }
             }
-        }
+        },
     })
 
 
