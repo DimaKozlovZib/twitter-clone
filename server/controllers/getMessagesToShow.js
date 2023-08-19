b1945a9aaa41332eab1e721cd836e68c5fce879c
Dima Kozlov <zibocinfinity@gmail.com>
2023-08-19 18:45:03 +0300

    функция для получения сообщений от людей на которых есть подписка

--- server/controllers/getMessagesToShow.js
+++ server/controllers/getMessagesToShow.js
@@ -1,6 +1,8 @@
-const { Op } = require("sequelize")
-const { Likes, Message } = require("../models/models")
+const { Op, NOW } = require("sequelize")
+const { Likes, Message, Friends, User } = require("../models/models")
 const { USER_MESSAGE } = require("../models/mongoModels")
+const { Sequelize } = require("../db")
+const moment = require("moment/moment")
 
 const deleteUnique = arr => arr
     .filter((item, index, arr) => (!(arr.lastIndexOf(item) === index) || !(arr.indexOf(item) === index)) && true)
@@ -23,7 +25,48 @@ class contentGeneration {
         //оставляем пользователей которым конравились два и больше (общих с this.userId) твита
         return deleteDuplicates(deleteUnique(usersGooGrade))
     }
+    async getMessageFromSubscriber() {
+        try {
+            //получаем людей которые подписанны на пользователя
+            const usersFromFriends = await User.findAll({
+                attributes: ['id'],
+                include: [{
+                    model: Friends,
+                    where: { userId: this.userId },
+                    attributes: [],
+                    required: true
+                }],
+            })
+            const subscribersId = usersFromFriends.map(i => i.id);
+            // возвращаем сообщения которые созданны не позже недели
+            const maxTime = moment().subtract(1, 'week').toDate();
+            const messages = await Message.findAll({
+                attributes: ['id'],
+                where: {
+                    userId: {
+                        [Op.or]: subscribersId
+                    },
+                    createdAt: {
+                        [Op.gt]: maxTime
+                    }
+                }
+            });
+            if (messages.length === 0) {
+                return []
+            }
+            const subscribersMessageId = messages.map(i => i.id);
+            //проверяем сколько раз пользователь видел найденные сообщения и фильтруем от трёх и больше раз
+            const maxShow = 2;
+            const verifiedMessages = await USER_MESSAGE.find({
+                id: { $in: subscribersMessageId }, showCount: { $lte: maxShow }
+            }).select('messageId').exec()
+            return verifiedMessages.map(i => i?.messageId);
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 //module.exports = contentGeneration
 
 const cl = new contentGeneration(9)
+cl.getMessageFromSubscriber()
